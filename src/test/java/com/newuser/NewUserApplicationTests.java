630053bf1ddc928151bbb6f8d176f43b3c7d09d9
Ike <patrickikechukwukalukalu@gmail.com>
2024-10-17 11:52:18 +0100

    NewUserApplicationTest Commented

--- src/test/java/com/newuser/NewUserApplicationTests.java
+++ src/test/java/com/newuser/NewUserApplicationTests.java
@@ -1,13 +1,13 @@
-package com.newuser;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NewUserApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.newuser;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NewUserApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
